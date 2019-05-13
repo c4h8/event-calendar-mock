736f16ea52c3650d80a08e741b05a6bc097a6a9e
Panu Liesiö <panu.liesio@aalto.fi>
2019-05-13 21:33:15 +0300

    delete obsolete files

--- src/mockData.js
+++ src/mockData.js
@@ -46,17 +46,72 @@ const mockData = [
     id: 'd',
   },
   {
-    title: 'mock event no date',
+    title: 'mock event nasdo date',
     category: 'tanssit',
     id: 'e'
   },
   {
-    title: 'Lasten Rönni',
+    title: 'Lasten Rasdasdönni',
     category: 'tapahtuma',
     time: '09:40',
     price: 3,
     date: '16.8.2019',
     id: 'f',
+    imgUrl: 'https://web.archive.org/web/20190204112740im_/http://www.ronninlava.com/images/artistit2018/2018-06-30_lastenpaiva.jpg'
+  },
+  {
+    title: 'Keskiviikkoasdasdtanssit',
+    category: 'tanssit',
+    price: 6,
+    date: '1.5.2019',
+    id: 'a1',
+    description: 'Perinteiset lavatanssit, livemusiikki'
+  },
+  {
+    title: 'Tanssit + livemusiiasdasdkki',
+    category: 'tanssit',
+    date: '6.5.2019',
+    id: 'b1',
+  },
+  {
+    title: 'Yölintasdu',
+    category: 'musiikki',
+    time: '20:00',
+    price: 7.25,
+    imgUrl: 'https://kahvilaravintolaleikari.fi/wp-content/uploads/bfi_thumb/ylintu-o34mrvkg6petggds3buubx2ggb4a2ozm4d2vk67zqw.jpg',
+    date: '10.6.2019',
+    id: 'g1',
+    description: 'Yölintu-orkesteri täyttää tänä vuonna kunnioitettavat 25 vuotta! Juhlavuoden ja juuri ilmestyneen albumin kunniaksi yhtye järjestää syksyllä 2018 kymmenen paikkakuntaa sisältävän Maailma on kauneimmillaan -nimeä kantavan konserttikiertueen. Konsertti on toivottu ja odotettu läpileikkaus Yölintu-orkesterin pitkästä historiasta.'
+  },
+  {
+    title: 'Rönnin peräkonttasdikirppis 2',
+    category: 'tapahtuma',
+    time: '10:00',
+    date: '20.6.2019',
+    id: 'dasd1',
+    imgUrl: 'https://web.archive.org/web/20190204112741im_/http://www.ronninlava.com/images/kuvat_2018/2018-05-26_perakonttikirppis.jpg',
+    description: 'Tuo tarpeettomat tavarasi myyntiin tai tule tekemään uusia hankintoja!Myyntipaikka on veloitukseton ja tapahtuma-aika on klo 10-15. Myyntipaikkaa ei tarvitse varata etukäteen. Tapahtuman järjestää Eräjärven Urheilijat ry.'
+  },
+  {
+    title: 'Livemusiikkia asdasd',
+    category: 'musiikki',
+    time: '10:00',
+    date: '23.7.2019',
+    id: 'c1',
+  },
+  {
+    title: 'mock eveasdsadnt asd',
+    category: 'tanssit',
+    date: '13.8.2019',
+    id: 'd1',
+  },
+  {
+    title: 'Lasten Rönni asd',
+    category: 'tapahtuma',
+    time: '09:40',
+    price: 3,
+    date: '16.8.2019',
+    id: 'f1',
     imgUrl: 'https://web.archive.org/web/20190204112740im_/http://www.ronninlava.com/images/artistit2018/2018-06-30_lastenpaiva.jpg'
   },
 ];
